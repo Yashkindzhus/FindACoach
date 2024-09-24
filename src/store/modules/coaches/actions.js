7b10e7a224ef5618494db0073bea7b067bfbd751
Nastia Yashnieva <57439255+Yashkindzhus@users.noreply.github.com>
2024-09-24 17:24:07 -0400

    finished DB fetches + caching

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -23,7 +23,11 @@ export default {
       id: userId
     });
   },
-  async loadCoaches(context) {
+  async loadCoaches(context, payload) {
+    if(!payload.forceRefresh && !context.getters.shouldUpdate) {
+      return;
+    }
+
     const response = await fetch('https://find-a-coach-5a451-default-rtdb.firebaseio.com/coaches.json');
     const responseData = await response.json();
 
@@ -36,6 +40,7 @@ export default {
 
     for (const key in responseData) {
       const coach = {
+        id: key,
         firstName: responseData[key].firstName,
         lastName: responseData[key].lastName,
         description: responseData[key].description,
@@ -47,5 +52,6 @@ export default {
     }
 
     context.commit('setCoaches', coaches);
+    context.commit('setFetchTimestamp');
   }
 };
